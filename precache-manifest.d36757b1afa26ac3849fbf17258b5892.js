6b0774af8fee18a7ea72dce2985b6a70c301196d
wasimakram-0909 <akramwasim0909@gmail.com>
2020-07-16 21:28:08 +0530

    Updates

---
+++ precache-manifest.d36757b1afa26ac3849fbf17258b5892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee67e03b8dec02fa11cf2eea158808ef",
+    "url": "/silverlabs_app/index.html"
+  },
+  {
+    "revision": "141dea6c7c33c83cff1e",
+    "url": "/silverlabs_app/static/css/main.8d58c91a.chunk.css"
+  },
+  {
+    "revision": "252c452dad05bb1a8b10",
+    "url": "/silverlabs_app/static/js/2.b5f7ed73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/silverlabs_app/static/js/2.b5f7ed73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141dea6c7c33c83cff1e",
+    "url": "/silverlabs_app/static/js/main.54bd3680.chunk.js"
+  },
+  {
+    "revision": "24148abba71478391f6f",
+    "url": "/silverlabs_app/static/js/runtime-main.3614e303.js"
+  }
+]);
